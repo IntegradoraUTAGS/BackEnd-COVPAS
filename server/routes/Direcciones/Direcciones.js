85d2220a744016e177ca218fef22371bca1629d5
AlexHM <alex13pks@gmail.com>
2020-03-11 14:38:35 -0600

    Añadida la ruta de direcciones

---
+++ server/routes/Direcciones/Direcciones.js
@@ -0,0 +1,22 @@
+/* jshint esversion: 8 */
+const Direccion = require('../../models/direccion');
+const express = require('express');
+const app = express();
+
+app.post('/registrar',(res,req) =>{
+    let dir = new Direccion({
+        strNombre: req.body.strNombre
+    });
+
+    new Direccion(dir).save().then((resp)=> {
+        return res.status(200).json({
+            ok: true,
+            cont: resp
+        });
+    }).catch((err)=> {
+        return res.status(400).json({
+            ok: false,
+            cont: err
+        });
+    });
+});
--- server/routes/index/index.js
+++ server/routes/index/index.js
@@ -6,5 +6,6 @@ app.use('/paseSalida',require('../paseSalida/Destinos/Destinos'));
 app.use('/persona',require('../Persona/login/login'));
 app.use('/persona',require('../Persona/Persona'));
 app.use('/paseSalida',require('../../routes/paseSalida/paseSalida'));
+app.use('/direcciones',require('../../routes/Direcciones/Direcciones'));
 
 module.exports = app;
