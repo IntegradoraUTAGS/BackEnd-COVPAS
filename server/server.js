aeedd229ab78b59dc0e7e1b73bed238633aee35e
AlexHM <alex13pks@gmail.com>
2020-03-23 15:21:56 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,16 +8,16 @@ const app = express();
 
 // Habilita CORS
 app.use((req, res, next) => {
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader(
-'Access-Control-Allow-Headers',
-'Origin, X-Requested-With, Content-Type, Accept, Authorization, token'
-);
-res.setHeader(
-'Access-Control-Allow-Methods',
-'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-);
-next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, token'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
 });
 
 //Parse application/x-www-form-urlencoded
